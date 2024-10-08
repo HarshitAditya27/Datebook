ada396988f6932fcbe71551fb6ea683b17471fac
Harshit Aditya <harshitaditya27@gmail.com>
2024-10-08 20:08:29 +0530

    Added availability component

---
+++ app/(main)/availability/page.jsx
@@ -0,0 +1,13 @@
+import { getUserAvailability } from "@/actions/availability";
+import React from "react";
+import AvailabilityLayout from "./layout";
+import { defaultAvailability } from "./data";
+import AvailabilityForm from "./_components/availability-form";
+
+async function AvailabilityPage() {
+  const availability = await getUserAvailability();
+  console.log(availability);
+  return <AvailabilityForm initialData={availability || defaultAvailability} />;
+}
+
+export default AvailabilityPage;
