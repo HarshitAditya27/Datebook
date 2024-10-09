4e4dc9bcb5b6d94c97589129ab2b0c63ea5fd2ba
Harshit Aditya <harshitaditya27@gmail.com>
2024-10-09 21:39:55 +0530

    Added meeting component along with meeting listings and cancellataion functionalities

--- actions/availability.js
+++ actions/availability.js
@@ -2,7 +2,6 @@
 import { db } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 
-//my code
 export async function getUserAvailability() {
   const { userId } = auth();
   if (!userId) {
@@ -50,58 +49,6 @@ export async function getUserAvailability() {
   return availabilityData;
 }
 
-//sc
-// export async function getUserAvailability() {
-//   const { userId } = auth();
-
-//   if (!userId) {
-//     throw new Error("Unauthorized");
-//   }
-
-//   const user = await db.user.findUnique({
-//     where: { clerkUserId: userId },
-//     include: {
-//       availability: {
-//         include: { days: true },
-//       },
-//     },
-//   });
-
-//   if (!user || !user.availability) {
-//     return null;
-//   }
-
-//   // Transform the availability data into the format expected by the form
-//   const availabilityData = { timeGap: user.availability.timeGap };
-
-//   [
-//     "monday",
-//     "tuesday",
-//     "wednesday",
-//     "thursday",
-//     "friday",
-//     "saturday",
-//     "sunday",
-//   ].forEach((day) => {
-//     const dayAvailability = user.availability.days.find(
-//       (d) => d.day === day.toUpperCase()
-//     );
-
-//     availabilityData[day] = {
-//       isAvailable: !!dayAvailability,
-//       startTime: dayAvailability
-//         ? dayAvailability.startTime.toISOString().slice(11, 16)
-//         : "09:00",
-//       endTime: dayAvailability
-//         ? dayAvailability.endTime.toISOString().slice(11, 16)
-//         : "17:00",
-//     };
-//   });
-
-//   return availabilityData;
-// }
-
-// my code
 export async function updateAvailability(data) {
   const { userId } = auth();
   if (!userId) {
